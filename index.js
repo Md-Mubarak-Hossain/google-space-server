f2365c5a38f9a2e7f74fb97db352a1f4c9a00a8a
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-12-30 18:53:05 +0600

    task filed added

--- index.js
+++ index.js
@@ -9,71 +9,66 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("task server!");
+    res.send("task server!");
 });
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@messengercluster.z2rf1zn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
-  try {
-    const database = client.db("programmingSchool");
-    const taskCollection = database.collection("task");
+    try {
+        const database = client.db("programmingSchool");
+        const taskCollection = database.collection("task");
 
-    app.get("/mytask", async (req, res) => {
-      const query = {};
-      const cursor = taskCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.get("/mytask/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await taskCollection.findOne(query);
-      res.send(result);
-    });
-    app.post("/mytask", async (req, res) => {
-      const user = req.body;
-      const result = await taskCollection.insertOne(user);
-      console.log(result);
-      res.send(result);
-    });
-    app.put("/mytask/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const filter = {
-        _id: ObjectId(id),
-      };
-      const task = req.body;
-      console.log(task);
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          name: taskname,
-          email,
-          image,
-          description,
-        },
-      };
-      const result = await taskCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
-      console.log(result);
-    });
-    app.delete("/mytask/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await taskCollection.deleteOne(query);
-      res.send(result);
-    });
-  } finally {
-    // await client.close();
-  }
+        app.get("/mytask", async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.get("/mytask/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.findOne(query);
+            res.send(result);
+        });
+        app.post("/mytask", async (req, res) => {
+            const user = req.body;
+            const result = await taskCollection.insertOne(user);
+            console.log(result);
+            res.send(result);
+        });
+        app.patch('/mytask/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const user = req.body;
+            const updateDoc = {
+                $set: {
+                    description: user.description,
+                    email: user.email,
+                    name: user.name,
+                    worker: user.worker,
+                    image: user.image,
+                }
+            }
+            const result = await taskCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+        app.delete("/mytask/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        });
+    } finally {
+        // await client.close();
+    }
 }
 run().catch((err) => console.error(err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
